29b1712a7d6f71cd3e7a6ece198dc264c3e817dc
Thevan Rajendran <thevanrajendran@gmail.com>
2021-10-05 09:58:07 +0800

    event2

--- event-handler/js/events.js
+++ event-handler/js/events.js
@@ -2,6 +2,7 @@ function transformUppercase(_val){
     return _val.toUpperCase();
 }
 
+// DOM event listener
 let elNamaPengguna = document.getElementById("namaPengguna")
 elNamaPengguna.addEventListener("keyup",function(){
     elNamaPengguna.value = transformUppercase(elNamaPengguna.value)
@@ -18,5 +19,6 @@ function checkPassword(){
     }
 }
 
+// Traditional DOM event handler
 let elKL = document.getElementById("katalaluan")
 elKL.onblur = checkPassword
