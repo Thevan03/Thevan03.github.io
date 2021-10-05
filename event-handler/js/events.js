703ab5a051988dd27d4fd441fba58622c83cf359
Thevan Rajendran <thevanrajendran@gmail.com>
2021-10-05 09:40:28 +0800

    event2

--- event-handler/js/events.js
+++ event-handler/js/events.js
@@ -6,3 +6,17 @@ let elNamaPengguna = document.getElementById("namaPengguna")
 elNamaPengguna.addEventListener("keyup",function(){
     elNamaPengguna.value = transformUppercase(elNamaPengguna.value)
 })
+
+function checkPassword(){
+    let elKatalaluan = document.getElementById("katalaluan")
+    let pass_val = elKatalaluan
+
+    if(pass_val.length > 5){
+        alert("Password check....OK!")
+    } else {
+        alert("Password too short!")
+    }
+}
+
+let elKL = document.getElementById("katalaluan")
+elKL.onblur = checkPassword
